889ab529f9a664d33152561a0a9bb0e0551cf636
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-04-20 15:21:12 +0300

    miles comment

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 km = 12.25  # this is Kilometer!
-mi = 7.38
+mi = 7.38   # this is Miles!
 
 mi_to_km = mi * 1.61
 km_to_mi = km / 1.61
 
 print(mi, "miles is", round(mi_to_km, 2), "kilometers")
 print(km, "kilometers is", round(km_to_mi, 2), "miles")
-
-# random comment
-# testing
-# maybe pull-request will work?
-# Github Actions activated
