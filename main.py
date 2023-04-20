23fa6aa090f59de56c1b20570a32ffc400dbf113
Hammy Wong <wonghammy6@gmail.com>
2023-04-20 16:03:02 +0300

    CICD-2 trying this commit

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 # mi is an abbreviation of miles
 km = 12.25
 mi = 7.38
-mi_to_km = mi * 1.61
+mi_to_km = mi * 1.61 #miles are converted to kilometers
 km_to_mi = km / 1.61
 
-print("This is an example about converting from miles to kilometers and from kilometers to miles!")
 print(mi, "miles is", round(mi_to_km, 2), "kilometers")
 print(km, "kilometers is", round(km_to_mi, 2), "miles")
 
-# Testing
+# random comment
+# testing
+# maybe pull-request will work?
+# Github Actions activated
