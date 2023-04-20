4d6ad29879dcf93fa6d44f4e9242a76e4e9a1e7c
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-04-20 16:00:02 +0300

    CICD-1 added comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 km = 12.25  # this is Kilometer!
 mi = 7.38   # this is Miles!
 
-mi_to_km = mi * 1.61
+mi_to_km = mi * 1.61 # kilometers converted to miles
 km_to_mi = km / 1.61
 
 print("This is an example about converting from miles to kilometers and from kilometers to miles!")
