f8af24e633d8e9b958213b2803cb946deaa0b219
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-04-20 15:24:22 +0300

    print more

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ mi = 7.38   # this is Miles!
 mi_to_km = mi * 1.61
 km_to_mi = km / 1.61
 
+print("This is an example about converting from miles to kilometers and from kilometers to miles!")
 print(mi, "miles is", round(mi_to_km, 2), "kilometers")
 print(km, "kilometers is", round(km_to_mi, 2), "miles")
