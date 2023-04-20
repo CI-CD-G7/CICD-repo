6acfaf901b1d8045e1ae54e5f5e77a0d87ef0f29
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-04-20 15:54:12 +0300

    test

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ km_to_mi = km / 1.61
 print("This is an example about converting from miles to kilometers and from kilometers to miles!")
 print(mi, "miles is", round(mi_to_km, 2), "kilometers")
 print(km, "kilometers is", round(km_to_mi, 2), "miles")
+
+# Testing
